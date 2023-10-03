1773bcce204240b6b3f2ac7cf78ecf2541a4f597
fsociety <musharafz2k3@gmail.com>
2023-10-03 23:02:20 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors:{
-        primary:'#4452BD',
-        secondary:'#807676'
-      }
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [],
